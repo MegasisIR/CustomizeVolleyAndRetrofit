c402546dbefe57881a891e8b98c163b8af7e6d94
[MehrdadTabesh] <[mehrdadit12@gmail.com]>
2020-08-24 12:26:22 +0430

    منابع اولیه پروژه

--- app/src/main/java/com/example/customizevolley/MainActivity.java
+++ app/src/main/java/com/example/customizevolley/MainActivity.java
@@ -1,91 +1,16 @@
 package com.example.customizevolley;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.android.volley.VolleyError;
-
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private ApiService apiService;
-    private StudentAdapter adapter;
-    private RecyclerView recyclerView;
-    private static final int ADD_STUDENT_REQUEST_ID = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        apiService = new ApiService(MainActivity.this, TAG);
-
-        Toolbar toolbar = findViewById(R.id.main_toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp);
-
-
-        View addNewStudentBtn = findViewById(R.id.fab_main);
-        addNewStudentBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AddNewStudentFormActivity.class);
-                startActivityForResult(intent, ADD_STUDENT_REQUEST_ID);
-            }
-        });
-
-        apiService.getStudent(new ApiService.ListStudentCallback() {
-            @Override
-            public void onSuccess(List<Student> students) {
-                adapter = new StudentAdapter(students);
-                recyclerView = findViewById(R.id.rv_main_listStudent);
-                recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, RecyclerView.VERTICAL, false));
-                recyclerView.setAdapter(adapter);
-            }
-
-            @Override
-            public void onError(VolleyError error) {
-
-            }
-        });
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == ADD_STUDENT_REQUEST_ID && resultCode == RESULT_OK && data != null) {
-            Student student = data.getParcelableExtra("student");
-            adapter.addStudentToRv(student);
-            recyclerView.smoothScrollToPosition(0);
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        apiService.cancel();
     }
 }
